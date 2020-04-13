f727becb3c3d830ed81b13b992a7245d69e985bc
patri <pabels@students.uni-mainz.de>
2020-04-13 22:41:01 +0200

    client gui done, gamelogic buggy

--- client.py
+++ client.py
@@ -3,19 +3,42 @@ from time import sleep
 from twisted.internet import reactor
 from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory, connectWS
 
-from gui import stack, num_to_let
+from gui import stack, num_to_let, cols, let_to_num, tk_cols
+from tkinter import *
+from tkinter import font as tkFont
+ 
+
 
 
 class ClientProtocol(WebSocketClientProtocol):
 
 	player_id = '0'
+	played_card = None
 
-	def makeShot(self, board):
-		shot = ''
-		while not shot in stack:
-			let_shot = input('shot? ')
-			shot = let_shot[0] + num_to_let[let_shot[1:]]
-		return shot
+	def clicked(self, card, window):
+		# print(card)
+		self.played_card.set(card)
+		window.destroy()
+		
+
+	def game_tick(self, cards):
+		window = Tk()
+		window.title("Rage")
+		window.geometry('1200x200')
+
+		self.played_card = StringVar()
+
+		for i, card in enumerate(cards.split()): # todo always returns last card
+			helv36 = tkFont.Font(family='Helvetica', size=36, weight=tkFont.BOLD) 
+			btn = Button(window, activebackground='white', fg='black', text=let_to_num[card[1]], bg=tk_cols[card[0]], font=helv36, command=lambda: self.clicked(card, window))
+			btn.grid(column=i, row=0)
+
+		window.mainloop()
+
+	def shot_made(self):
+		shot = self.played_card.get()
+		# print(shot)
+		self.sendMessage(shot.encode('utf8'))
 
 	def onOpen(self):
 		print('CONNECTED TO SERVER')
@@ -29,9 +52,13 @@ class ClientProtocol(WebSocketClientProtocol):
 			else:
 				if msg[0] == self.player_id:
 					print(msg)
-					board = msg.split(':')[1]
-					shot = self.makeShot(board)
-					self.sendMessage(shot.encode('utf8'))
+					turn, board, cards = msg.split('\n')
+					board = board.split(',')[0].split(':')[1].strip()
+					cards = cards.split(':')[1].strip()
+
+					self.game_tick(cards)
+					print(self.played_card.get())
+					self.sendMessage(self.played_card.get().encode('utf8'))					
 
 
 
@@ -39,3 +66,5 @@ factory = WebSocketClientFactory('ws://127.0.0.1:9000')
 factory.protocol = ClientProtocol
 connectWS(factory)
 reactor.run()
+
+
--- server.py
+++ server.py
@@ -6,13 +6,17 @@ from twisted.web.server import Site
 from twisted.web.static import File
 from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerFactory, listenWS
 
-from gui import game_tick, init
+from gui import game_tick, init, stack
 from twisted.internet.task import LoopingCall
 
-'''
-TODO
-- 
-'''
+from random import shuffle
+from tkinter import *
+
+tk = Tk()
+tk.withdraw()
+
+
+
 
 def new_game():
 	print('--- NEW GAME ---')
@@ -22,30 +26,61 @@ def new_game():
 
 class ServerProtocol(WebSocketServerProtocol):
 
-	def read_balls(self): # todo save top card of stack and state of game somewhere and load it here
-		# if os.path.exists('board.txt'):
-		# 	with open('board.txt', 'r') as f:
-		# 		return f.read()
-		# else:
-		# 	return ''
-		return 'r1, 0:3/1:0/2:0'
+	cards_per_client = IntVar(value=0) # not working yet
 
-	def read_cards(self): # todo save cards in dict and load the right ones here
-		# self.factory.turn
-		return 'r3 g2 b9'
+	def deal_cards(self, num_cards):
+		print('>>> DEAL:', num_cards)
+		s = stack
+		shuffle(s)
+		for c in self.factory.clients:
+			self.factory.clients[c]['cards'] = ' '.join(s[:num_cards])
+			s = s[num_cards:]
+		self.factory.shots = [s.pop()]
+
+		self.cards_per_client.set(num_cards)
+
+	def read_board(self):
+		top_card = self.factory.shots[-1]
+		statistics = self.factory.statistics
+		return top_card + ', ' + statistics
+
+	def read_cards(self): 
+		for c in self.factory.clients:
+			if self.factory.turn == self.factory.clients[c]['id']:
+				return self.factory.clients[c]['cards']
+		return 'r1 r2 r3'
+
+	def round_over(self):
+		for c in self.factory.clients:
+			if len(self.factory.clients[c]['cards'].split()) > 0:
+				return False
+		return True
+
 
 	def render(self, shot):
 		self.factory.shots.append(shot)
+		for c in self.factory.clients:
+			if self.factory.turn == self.factory.clients[c]['id']:
+				cards = self.factory.clients[c]['cards'].split()
+				cards.remove(shot)
+				self.factory.clients[c]['cards'] = ' '.join(cards)				
 		game_tick(self.factory.shots)
 
 	def broadcastBoard(self):
-		self.factory.broadcast(self.factory.turn + "'s Turn:\n" + 'Board: ' + self.read_balls() + '\nCards: ' + self.read_cards())
+		self.factory.broadcast(self.factory.turn + "'s Turn:\n" + 'Board: ' + self.read_board() + '\nCards: ' + self.read_cards())
+
+	def var_changed(self):
+		if self.cards_per_client == 0:
+			self.deal_cards(i)
+			self.broadcastBoard()
 
 	def onOpen(self):
 		self.factory.register(self)
 		if len(self.factory.clients) == max_clients:
 			new_game()
-			self.broadcastBoard()
+			# todo interactive mode
+			for i in [1,2,3,4,5,6,7,8,9,10]:
+				self.cards_per_client.trace(mode="w", callback=lambda: self.var_changed(i))
 			
 
 	def onMessage(self, payload, isBinary):
@@ -53,7 +88,9 @@ class ServerProtocol(WebSocketServerProtocol):
 			shot = payload.decode('utf8')
 			print(self.factory.turn + ': ' + shot)
 			self.render(shot)
-			self.factory.turn = str((int(self.factory.turn)+1)%len(self.factory.clients)) # not always the same one to start
+			if self.round_over():
+				self.cards_per_client.set(0)
+			self.factory.turn = str((int(self.factory.turn)+1)%len(self.factory.clients)) # todo not always the same one to start
 			self.broadcastBoard()
 
 	def connectionLost(self, reason): # todo deconnect clients if server crashes
@@ -65,21 +102,22 @@ class ServerFactory(WebSocketServerFactory):
 
 	def __init__(self, url):
 		WebSocketServerFactory.__init__(self, url)
-		self.clients = [] # todo dict with id, cards, ...
+		self.clients = {}
 		self.turn = '0'
 		self.shots = []
+		self.statistics = '0:3/1:0/2:0' # todo
 
 	def register(self, client): # todo reconnection not depending on clients length bc duplicate client_ids
 		if client not in self.clients:
 			client_id = str(len(self.clients))
 			hello_msg = 'HELLO '+client_id
 			client.sendMessage(hello_msg.encode('utf8'))
-			self.clients.append(client)
+			self.clients[client] = {'id': client_id, 'cards': ''}
 			print('CONNECT')
 
 	def unregister(self, client):
 		if client in self.clients:
-			self.clients.remove(client)
+			del self.clients[client]
 			print('DECONNECT')
 
 	def broadcast(self, msg):
